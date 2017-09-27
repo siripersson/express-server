a683db78c95dabcc25af24ecc286026574275db7
siripersson <siri.persson1@gmail.com>
2017-09-27 12:19:27 +0200

    Add server request and respond

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express= require('express') // importera express och spara i en variabel
 
 const app =express() // Skapa en express app
 
+//app.get('/', function(req,res){
+app.get('/', (req,res) => {
+	console.log("Nu är vi här")
+	res.send("Hello World!")
+
+
+})
 // Koppla appen så den lyssnar på trafik
 // Det gör vi genom att ange vilken port den ska lyssna på
 app.listen(3000, function(){
